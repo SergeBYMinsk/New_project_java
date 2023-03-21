d9464c7712e83b1edd29d0ef08549896afb1666f
sergebyminsk <sterlikovsergey@iclous.com>
2023-03-22 02:19:01 +0300

    new method Scanner

--- src/newClass.java
+++ src/newClass.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class newClass {
     public static void main(String[] args) {
+
+        System.out.println("new class");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str);
+
 
     }
 }
